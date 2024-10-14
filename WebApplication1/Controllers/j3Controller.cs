bff29bda7ae24814062c46e5a18039ef5d97ed11
dhruvshah28 <dhruvshah2811ds@gmail.com>
2024-10-14 17:05:23 -0400

    upload assignment-2

--- WebApplication1/Controllers/j3Controller.cs
+++ WebApplication1/Controllers/j3Controller.cs
@@ -7,6 +7,31 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class j3Controller : ControllerBase
     {
+
+        /// <summary>
+        /// An Http Get request that takes use inputs in form of sequences(five number code) 
+        /// that would be saved as list of strings and then it will give out instructions 
+        /// including direction from first two digits and number of steps to take from the 
+        /// last three digits.
+        /// </summary>
+        /// <param name="sequences">List of five-digit sequences given by user</param>
+        /// <remarks>
+        /// Each instruction need to be five digits.  
+        /// The first two digits are the direction of the code:
+        /// If their addition is odd than the direction is "left".
+        /// Or if their sum is even and not zero, the direction is "right".
+        /// Or if their sum is zero, the direction remains the same as the previous instruction.
+        /// The last three digits represent the number of steps from the code.
+        /// And if the sequence is "99999" process will end and won't take any other input.
+        /// </remarks>
+        /// <returns>A list of decoded instructions that consists of direction and steps.</returns>
+        /// <example>
+        /// GET api/J3/code?sequences=57234&sequences=00907&sequences=34100&sequences=99999  ->  
+        /// right 234
+        /// right 907
+        /// left 100
+        /// </example>
+
         [HttpGet(template: "secretinstructions")]
         public string secretinstructions([FromQuery] List<string> lnum)
         {
